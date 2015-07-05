a93b0264dd3304a993407b0513a47cf7b3608c54
marshi <mashi3648@gmail.com>
2015-07-05 23:52:18 +0900

    ホスト登録機能を削除. POSTリクエスト時にホスト名も登録する.

--- src/main/scala/controller/HostController.scala
+++ src/main/scala/controller/HostController.scala
@@ -2,8 +2,6 @@ package controller
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
-import com.twitter.finatra.request.QueryParam
-import com.twitter.finatra.validation.NotEmpty
 import service.{HostBranchService, HostMachineService}
 
 /**
@@ -15,23 +13,6 @@ class HostController extends Controller {
     val hostMachineService = new HostMachineService
     val hostList = hostMachineService.fetchHostSeq()
     response.ok.view("host/list.mustache", hostList)
-  }
-
-  get("/host/register") { request: Request =>
-    response.ok.view("host/register.mustache", null)
-  }
-
-  post("/host/register") { request: Request =>
-    val hostName = request.params.get("host_name")
-    val hostMachineService = new HostMachineService
-    try {
-      hostMachineService.registerHostMachine(hostName)
-      val hostList = hostMachineService.fetchHostSeq()
-      response.ok.view("host/list.mustache", hostList);
-    } catch {
-      case e: IllegalArgumentException => response.badRequest.view("/host/register.mustache", null);
-      case e: Exception => response.internalServerError.view("/host/register.mustache", null)
-    }
   }
 
   delete("/host/delete") { request: Request =>
